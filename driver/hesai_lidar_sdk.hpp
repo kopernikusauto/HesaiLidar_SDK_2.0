95c4e98838c4d821a8b84ada250f345d2b1391f6
gsvirskiy <gsvirskiy@kopernikusauto.com>
2025-11-17 15:19:33 +0100

    Update version

--- driver/hesai_lidar_sdk.hpp
+++ driver/hesai_lidar_sdk.hpp
@@ -63,10 +63,10 @@ public:
   int multi_send_rate_count_ = 0;
   HesaiLidarSdk() {
 #ifdef USE_CUDA
-    std::cout << "-------- Hesai Lidar SDK Gpu V" << 2 << "." << 0 << "." << 11 << " --------" << std::endl;
+    std::cout << "-------- Hesai Lidar SDK Gpu V" << 2 << "." << 0 << "." << 13 << " --------" << std::endl;
     gpu_parser_ptr_ = nullptr;
 #else
-    std::cout << "-------- Hesai Lidar SDK V" << 2 << "." << 0 << "." << 11 << " --------" << std::endl;
+    std::cout << "-------- Hesai Lidar SDK V" << 2 << "." << 0 << "." << 13 << " --------" << std::endl;
 #endif
     runing_thread_ptr_ = nullptr;
     lidar_ptr_ = nullptr;
